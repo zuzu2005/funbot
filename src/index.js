be04b6ec200cc974ca7d8fe271c36c2c81b276f6
zuzu2005 <udly@sina.com>
2017-07-12 22:58:16 +0800

    three examples1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Examples1 from './examples1';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Examples1 />, document.getElementById('root'));
 registerServiceWorker();
